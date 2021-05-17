bccacd7b342b0f1b9e794fab81c07b683331f12b
sensedata1 <lordben@gmail.com>
2021-05-17 22:57:57 +0100

    add .command for container

--- run-afd-container.sh
+++ run-afd-container.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 clear
+echo "Pulling image..."
 OUT=$(docker pull sensedata1/afd:latest)
 echo "${OUT}"
-if [[ ${OUT} == *"up to date"* ]]; then
+if [[ ${OUT} == *"up to date"* || *"newer image"* ]]; then
     echo
     read -p "Drop your AJ downloads folder here... " AJTEMP
     echo ${AJTEMP}
-    docker run \
+    docker run --rm \
            -v "${AJTEMP}":/AJTEMP \
            -it sensedata1/afd:latest
 else
